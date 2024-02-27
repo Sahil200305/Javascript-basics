8caa61cc55ef2d08ae83464b9153d4be93dab7df
Sahil200305 <149478523+Sahil200305@users.noreply.github.com>
2024-02-27 21:17:18 +0000

    investigation

---
+++ basics/basic2.js
@@ -0,0 +1,11 @@
+const accountId = 576675
+let accountEmail = "abc@hotmail.com"
+var accountPassword = 1233
+accountCity = "Dehradun"
+
+// without use any constrain email password  and city will change//
+accountEmail = "bhs@hhh.com"
+accountPassword = 23234
+accountCity = "delhi"
+
+console.table([accountId,accountEmail,accountPassword,accountCity])
