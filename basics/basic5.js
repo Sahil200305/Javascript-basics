280ddb36d98748624355d950ea86a9418c74a452
Sahil200305 <149478523+Sahil200305@users.noreply.github.com>
2024-02-28 17:27:49 +0000

    Operations

---
+++ basics/basic5.js
@@ -0,0 +1,30 @@
+//console.log(2 + 2)
+//console.log(2 - 2)
+//console.log(2*2)
+//console.log(2/2)
+//console.log(2%2)
+
+//console.log("2" + 1)
+//console.log(2 + "1")
+//console.log("2" + 1 + 1)
+//console.log(2 + 1 + "2")
+
+
+//console.log(2 > 5)
+//console.log(2 < 3)
+//console.log(2 <= 5)
+//console.log(2 >= 3)
+
+console.log("2" >=1)
+console.log("02">= 3)
+
+console.log(null > 0)
+console.log(null >= 0)
+console.log(null <= 0)
+console.log(null != 0)
+
+
+
+
+
+
