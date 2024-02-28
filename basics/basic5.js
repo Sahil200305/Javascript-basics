e5e372153b98a53045faa986541ecda97fe85f06
Sahil200305 <149478523+Sahil200305@users.noreply.github.com>
2024-02-28 17:33:19 +0000

    Operations

--- basics/basic5.js
+++ basics/basic5.js
@@ -1,19 +1,19 @@
-//console.log(2 + 2)
-//console.log(2 - 2)
-//console.log(2*2)
-//console.log(2/2)
-//console.log(2%2)
+console.log(2 + 2)
+console.log(2 - 2)
+console.log(2*2)
+console.log(2/2)
+console.log(2%2)
 
-//console.log("2" + 1)
-//console.log(2 + "1")
-//console.log("2" + 1 + 1)
-//console.log(2 + 1 + "2")
+console.log("2" + 1)
+console.log(2 + "1")
+console.log("2" + 1 + 1)
+console.log(2 + 1 + "2")
 
 
-//console.log(2 > 5)
-//console.log(2 < 3)
-//console.log(2 <= 5)
-//console.log(2 >= 3)
+console.log(2 > 5)
+console.log(2 < 3)
+console.log(2 <= 5)
+console.log(2 >= 3)
 
 console.log("2" >=1)
 console.log("02">= 3)
@@ -23,6 +23,10 @@ console.log(null >= 0)
 console.log(null <= 0)
 console.log(null != 0)
 
+console.log("2" === 2)
+let num1 = 23
+num1 ++;
+console.log(num1)
 
 
 
