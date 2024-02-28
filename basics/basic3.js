261ed06fbf5a3c5687ea3cbec66797c363efb829
Sahil200305 <149478523+Sahil200305@users.noreply.github.com>
2024-02-28 13:49:39 +0000

    Data Types

---
+++ basics/basic3.js
@@ -0,0 +1,11 @@
+let name = ("SahilRouthan")
+let age = 18
+let isLoggedIn = true
+let temperature = null
+let city = undefined
+
+console.log(typeof"name")
+console.log(typeof age)
+console.log(typeof isLoggedIn)
+console.log(typeof temperature)
+console.log(typeof city)
