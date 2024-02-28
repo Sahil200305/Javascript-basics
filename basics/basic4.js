71628a722b1f2e3d9bc7af4108c72f624cb6555c
Sahil200305 <149478523+Sahil200305@users.noreply.github.com>
2024-02-28 14:32:32 +0000

    datatype conversion operation

---
+++ basics/basic4.js
@@ -0,0 +1,32 @@
+let score = "33"
+
+//console.log(typeof score)
+//console.log(typeof (score))//
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber)
+
+let age = 33
+
+//console.log(typeof age)
+let ageInString = String(age)
+
+//console.log(typeof ageInString)
+
+let code = null
+console.log(typeof code)
+
+let codeInNumber = Number(code)
+
+console.log(typeof codeInNumber)
+console.log(codeInNumber)
+
+
+
+let id = null
+//console.log(typeof id)
+
+let uid = undefined
+//console.log(typeof uid)
+
+
