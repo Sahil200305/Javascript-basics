06b865f4ee2e6531f286df538f8cb27d312f4c98
Sahil200305 <149478523+Sahil200305@users.noreply.github.com>
2024-03-25 20:20:42 +0000

    Strings In JavaScript

---
+++ basics/basic7.js
@@ -0,0 +1,33 @@
+const name = "Sahil"
+const city = "Dehradun"
+//console.log(name + city) ---- Old Syntax
+//console.log(`Hello My Name is ${name} and i am currently in ${city}`) --- Modern Syntax
+
+const GameName = 'Sahil-Routhan'
+//console.log(GameName.length) --- for length 
+
+//console.log(GameName.charAt(2)) --- charcter at index 2.
+
+//console.log(GameName.toUpperCase()) ---- to write in uppercase .
+
+//console.log(GameName.indexOf('a')) ----- index of character.
+
+const NewString = GameName.substring(0,4)
+//console.log(NewString) //
+
+const StringOne = "     Sahil     "
+//console.log(StringOne) ----- output = "       Sahil         ". 
+//console.log(StringOne.trim()) ----- output = "Sahil".
+
+//console.log(GameName.slice(-6,-4)) ---- output = ou
+
+const Url = "wwww.hdwhw.com/abcd%20cvbbc"
+
+//console.log(Url.replace('%20', '-')) ---- replace one string to another.
+
+
+//console.log(GameName.includes('Sahil')) ----- check string is present or not. Output = True.
+
+//console.log(GameName.includes('Shail')) ------ Output = False.
+
+//console.log(GameName.split('-')) ----- Output = ['Sahil' , 'Routhan']
